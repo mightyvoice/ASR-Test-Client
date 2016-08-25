b389145e8d87728f2e073f571b3113541f371782
Ji Li <rpbloom@gmail.com>
2016-08-24 18:58:28 -0700

    can perform google asr by connecting to the server

--- app/src/main/java/com/example/lj/asrttstest/CloudASRActivity.java
+++ app/src/main/java/com/example/lj/asrttstest/CloudASRActivity.java
@@ -32,36 +32,6 @@ import java.util.List;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-/////////test git
-import com.example.lj.asrttstest.info.AppInfo;
-import com.nuance.dragon.toolkit.audio.AudioChunk;
-import com.nuance.dragon.toolkit.audio.AudioType;
-import com.nuance.dragon.toolkit.audio.SpeechDetectionListener;
-import com.nuance.dragon.toolkit.audio.pipes.ConverterPipe;
-import com.nuance.dragon.toolkit.audio.pipes.EndPointerPipe;
-import com.nuance.dragon.toolkit.audio.pipes.OpusEncoderPipe;
-import com.nuance.dragon.toolkit.audio.pipes.SpeexEncoderPipe;
-import com.nuance.dragon.toolkit.audio.sources.BurstFileRecorderSource;
-import com.nuance.dragon.toolkit.audio.sources.MicrophoneRecorderSource;
-import com.nuance.dragon.toolkit.audio.sources.RecorderSource;
-import com.nuance.dragon.toolkit.audio.sources.StreamingFileRecorderSource;
-import com.nuance.dragon.toolkit.calllog.CalllogManager;
-import com.nuance.dragon.toolkit.calllog.CalllogManager.CalllogDataListener;
-import com.nuance.dragon.toolkit.calllog.CalllogSender;
-import com.nuance.dragon.toolkit.calllog.CalllogSender.SenderListener;
-import com.nuance.dragon.toolkit.calllog.SessionEvent;
-import com.nuance.dragon.toolkit.calllog.SessionEventBuilder;
-import com.nuance.dragon.toolkit.cloudservices.CloudConfig;
-import com.nuance.dragon.toolkit.cloudservices.CloudServices;
-import com.nuance.dragon.toolkit.cloudservices.DictionaryParam;
-import com.nuance.dragon.toolkit.cloudservices.recognizer.CloudRecognitionError;
-import com.nuance.dragon.toolkit.cloudservices.recognizer.CloudRecognitionResult;
-import com.nuance.dragon.toolkit.cloudservices.recognizer.CloudRecognizer;
-import com.nuance.dragon.toolkit.cloudservices.recognizer.RecogSpec;
-import com.nuance.dragon.toolkit.data.Data;
-import com.nuance.dragon.toolkit.util.Logger;
-import com.nuance.dragon.toolkit.vocon.ParamSpecs;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -70,21 +40,6 @@ public class CloudASRActivity extends AppCompatActivity
 {
     private static  final int VOICE_RECOGNITION_REQUEST_CODE = 1001;
 
-    private CloudServices      _cloudServices;
-    private CloudRecognizer    _cloudRecognizer;
-    private RecorderSource<AudioChunk>recorder;
-    private ConverterPipe<AudioChunk, AudioChunk> _encoder;
-    private SpeexEncoderPipe speexPipe;
-    private EndPointerPipe endpointerPipe;
-
-    // call log feature
-    private SessionEvent       _appSessionLeadEvent;
-    private String             _appLeadSessionId;
-    private CalllogSender      _calllogSender;
-    private AudioType          _audioType;
-    private TTSService         _ttsService;
-
-    //View
     private EditText editIpView;
     private Button connectToServerButton;
     private Button startGoogleAsrButton;
@@ -92,12 +47,14 @@ public class CloudASRActivity extends AppCompatActivity
     private TextView resultTextView;
 
     /* Connect to the server */
-    private String serverIP = "10.118.115.173";
-    private final int serverPort = 13458;
+    private String serverIP = "192.168.2.79";
+    private final int serverPort = 14458;
     private Socket clientSocket;
     private BufferedWriter clientWriter;
     private BufferedReader clientReader;
     private Handler severMessageHandler;
+    private String messageFromServer = "";
+    private String googleAsrResult = "";
 
     /**
      * Called when the activity is first created.
@@ -116,7 +73,7 @@ public class CloudASRActivity extends AppCompatActivity
         resultTextView = (TextView) findViewById(R.id.cloudResultEditText);
 
         editIpView.setInputType(InputType.TYPE_CLASS_NUMBER);
-        editIpView.setText("10.118.116.218");
+        editIpView.setText("192.168.2.79");
         startNuanceAsrButton.setEnabled(false);
         startGoogleAsrButton.setEnabled(false);
 
@@ -128,6 +85,19 @@ public class CloudASRActivity extends AppCompatActivity
             }
         });
 
+        startGoogleAsrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new ClientSendThread("start")).start();
+                try {
+                    Thread.sleep(700);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startGoogleASR(getCurrentFocus());
+            }
+        });
+
         severMessageHandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
@@ -135,288 +105,24 @@ public class CloudASRActivity extends AppCompatActivity
                 if (msg.what == 0) {
                     startGoogleASR(getCurrentFocus());
                 } else if (msg.what == 1) {
-                    onClickStartNuanceASR();
+                    resultTextView.setText("Msg from Server: "+messageFromServer);
+                    startGoogleAsrButton.performClick();
                 } else if (msg.what == 2) {
                     resultTextView.setText("Successful connect to server");
                     connectToServerButton.setEnabled(false);
+                    startGoogleAsrButton.setEnabled(true);
                 } else if (msg.what == 3) {
                     resultTextView.setText("Fail to connect to server");
                     connectToServerButton.setEnabled(true);
+                } else if (msg.what == 4){
+                    resultTextView.setText("Finish all audio Files");
+//                    connectToServerButton.setEnabled(true);
+                    startGoogleAsrButton.setEnabled(false);
                 }
             }
         };
 
-        _audioType = AudioType.SPEEX_WB;
-
-        reCreateCloudRecognizer();
     }
-
-    private void onClickStartNuanceASR() {
-        startNuanceAsrButton.setEnabled(false);
-        resultTextView.setText("");
-
-        String resultmodeName = "No Partial Results";
-
-        // Set-up audio chaining
-        recorder = new MicrophoneRecorderSource(AudioType.PCM_16k);
-//                curAudioFileID++;
-//                int resourceID = (getResources().getIdentifier("audio" + String.valueOf(curAudioFileID) + "_16k_pcm", "raw", getPackageName()));
-//                Log.d("sss", " resID: "+new Integer(resourceID).toString());
-//                Log.d("sss", "audio" + String.valueOf(curAudioFileID) + "_16k");
-//                recorder = new StreamingFileRecorderSource(
-//                        AudioType.PCM_16k,
-//                        400,
-//                        getResources().getIdentifier("audio" + String.valueOf(curAudioFileID) + "_16k_pcm", "raw", getPackageName()),
-//                        getApplicationContext());
-////                        googleAudioFile.getAbsolutePath(),
-////                        true);
-//                Log.d("sss", " resId: " + getResources().getIdentifier("audio" + String.valueOf(curAudioFileID) + "_16k_pcm", null, getPackageName()));
-//                Log.d("sss", "audio type: " + recorder.getAudioType());
-//                Log.d("sss", "chunks: " + recorder.getChunksAvailable());
-//                Log.d("sss", "isempty: " + recorder.getChunksAvailable());
-
-        speexPipe = new SpeexEncoderPipe();
-        endpointerPipe = new EndPointerPipe(new SpeechDetectionListener() {
-            @Override
-            public void onStartOfSpeech() {
-                resultTextView.setText("Start of Speech...");
-            }
-
-            @Override
-            public void onEndOfSpeech() {
-                resultTextView.setText("End of Speech...");
-                _cloudRecognizer.processResult();
-                startNuanceAsrButton.setEnabled(true);
-                stopRecording();
-                new Thread(new ClientSendThread(getAsrResultJsonStr(0, "###"))).start();
-            }
-        });
-
-        // Start recording and recognition
-        recorder.startRecording();
-        speexPipe.connectAudioSource(recorder);
-        endpointerPipe.connectAudioSource(speexPipe);
-        _cloudRecognizer.startRecognition(createNuanceRecogSpec(resultmodeName),
-                endpointerPipe,
-                new CloudRecognizer.Listener() {
-                    @Override
-                    public void onResult(CloudRecognitionResult result) {
-                        java.lang.String topResult = parseNuanceResults(result);
-                        if (topResult != null) {
-                            resultTextView.setText(topResult);
-                            new Thread(new ClientSendThread(getAsrResultJsonStr(2, topResult))).start();
-                        }
-                        startNuanceAsrButton.setEnabled(true);
-                    }
-
-                    @Override
-                    public void onError(CloudRecognitionError error) {
-//                                resultTextView.setText(error.toString());
-                        String err = error.toJSON().toString();
-                        resultTextView.setText("speech not recognized");
-                        Log.d("sss", err);
-                        startNuanceAsrButton.setEnabled(true);
-                        new Thread(new ClientSendThread(getAsrResultJsonStr(2, "Error"))).start();
-                    }
-
-                    @Override
-                    public void onTransactionIdGenerated(String transactionId) {
-                    }
-                });
-
-        if (_appSessionLeadEvent != null) {
-            SessionEventBuilder eventBuilder = _appSessionLeadEvent.createChildEventBuilder("cloud recognition");
-            eventBuilder.putString("start", "recognition started");
-            eventBuilder.commit();
-        }
-    }
-
-    private String getAsrResultJsonStr(int type, String result){
-        JSONObject tmp = new JSONObject();
-        try {
-            if(type == 0) tmp.put("result", "###");
-            else tmp.put("result", result);
-            if (type == 1) {
-                tmp.put("type", "google");
-            }
-            else if (type == 2){
-                tmp.put("type", "nuance");
-            }
-            else{
-                tmp.put("type", "end");
-            }
-        }catch (JSONException e){
-            e.printStackTrace();
-            return "\n";
-        }
-        return tmp.toString()+"\n";
-    }
-
-    private void stopRecording() {
-        if (endpointerPipe != null) {
-            endpointerPipe.disconnectAudioSource();
-            endpointerPipe = null;
-        }
-
-        if (speexPipe != null) {
-            speexPipe.disconnectAudioSource();
-            speexPipe = null;
-        }
-
-        if (recorder != null) {
-            recorder.stopRecording();
-            recorder = null;
-        }
-    }
-
-    @Override
-    public void onDestroy()
-    {
-        super.onDestroy();
-
-        if (recorder != null)
-            recorder.stopRecording();
-        recorder = null;
-
-        if (_encoder != null) {
-            _encoder.disconnectAudioSource();
-            _encoder.release();
-        }
-        _encoder = null;
-
-        if (_cloudRecognizer != null)
-            _cloudRecognizer.cancel();
-        _cloudRecognizer = null;
-
-        if (_cloudServices != null)
-            _cloudServices.release();
-        _cloudServices = null;
-    }
-
-    private RecogSpec createNuanceRecogSpec(String resultModeName) {
-        // Create a sample recognition spec. based on the "NVC_ASR_CMD"
-        Data.Dictionary settings = new Data.Dictionary();
-        settings.put("dictation_type", "dictation");
-        settings.put("dictation_language", "eng-USA");
-        if (_appLeadSessionId != null)
-            settings.put(CalllogManager.CALLLOG_APP_TRANSACTION_REF_EVENT, _appLeadSessionId);
-        RecogSpec retRecogSpec = new RecogSpec("NVC_ASR_CMD", settings, "AUDIO_INFO");
-
-        // Also, add necessary "REQUEST_INFO" parameter
-        Data.Dictionary requestInfo = new Data.Dictionary();
-        requestInfo.put("start", 0);
-        requestInfo.put("end", 0);
-        requestInfo.put("text", "");
-
-        if (resultModeName.equals("Utterance Detection Default"))
-            requestInfo.put("intermediate_response_mode", "UtteranceDetectionWithCompleteRecognition");
-        else if (resultModeName.equals("Utterance Detection Very Aggressive")) {
-            requestInfo.put("intermediate_response_mode", "UtteranceDetectionWithCompleteRecognition");
-            requestInfo.put("utterance_detection_silence_duration", "VeryAggressive");
-        }
-        else if (resultModeName.equals("Utterance Detection Aggressive")) {
-            requestInfo.put("intermediate_response_mode", "UtteranceDetectionWithCompleteRecognition");
-            requestInfo.put("utterance_detection_silence_duration", "Aggressive");
-        }
-        else if (resultModeName.equals("Utterance Detection Average")) {
-            requestInfo.put("intermediate_response_mode", "UtteranceDetectionWithCompleteRecognition");
-            requestInfo.put("utterance_detection_silence_duration", "Average");
-        }
-        else if (resultModeName.equals("Utterance Detection Conservative")) {
-            requestInfo.put("intermediate_response_mode", "UtteranceDetectionWithCompleteRecognition");
-            requestInfo.put("utterance_detection_silence_duration", "Conservative");
-        }
-        else if (resultModeName.equals("Streaming Results")) {
-            requestInfo.put("intermediate_response_mode", "NoUtteranceDetectionWithPartialRecognition");
-            _cloudRecognizer.setResultCadence(500);
-        }
-
-        retRecogSpec.addParam(new DictionaryParam("REQUEST_INFO", requestInfo));
-
-        return retRecogSpec;
-    }
-
-    private String parseNuanceResults(CloudRecognitionResult cloudResult)
-    {
-        Data.Dictionary processedResult = cloudResult.getDictionary();
-
-        // Parse results based on the "NVC_ASR_CMD"
-        //java.lang.String prompt = ((Data.String)results.get("prompt")).Value;
-        //Data.Sequence transcriptions = (Data.Sequence) results.get("transcriptions");
-        //Data.Sequence confidences = (Data.Sequence) results.get("confidences");
-
-        // The processed result has "prompt" == "warning", "transcriptions" == "choices"
-        if (processedResult == null || processedResult.getString("prompt") == null )
-            return null;
-
-        java.lang.String prompt = processedResult.getString("prompt").value;
-        Data.Sequence transcriptions = processedResult.getSequence("transcriptions");
-        //Data.Sequence scores = processedResult.getSequence("confidences");
-
-        int len = transcriptions.size();
-        //len = (confidences.size() == len) ? len : 0;
-
-        ArrayList<String> sentences = new ArrayList<java.lang.String>();
-
-        for (int idx = 0; idx < len; idx++) {
-            Data.String text = transcriptions.getString(idx);
-            //Data.Integer score = scores.getInt(idx);
-
-            sentences.add(text.value);
-        }
-
-        return (len == 0 ? prompt : sentences.get(0));
-    }
-
-    private void reCreateCloudRecognizer()
-    {
-        if (_cloudRecognizer != null)
-            _cloudRecognizer.cancel();
-        _cloudRecognizer = null;
-
-        if (_cloudServices != null)
-            _cloudServices.release();
-        _cloudServices = null;
-
-        // Cloud services initialization
-        _cloudServices = CloudServices.createCloudServices(CloudASRActivity.this,
-                new CloudConfig(AppInfo.Host, AppInfo.Port, AppInfo.AppId, AppInfo.AppKey, _audioType, _audioType, false));
-
-        _calllogSender = CalllogManager.createCalllogSender(new SenderListener() {
-
-            @Override
-            public void succeeded(byte[] data) {
-                Logger.debug(this, "call log data: " + data + " is sent");
-            }
-
-            @Override
-            public void failed(short errorCode, byte[] data) {
-                Logger.debug(this, "call log data: " + data + " failed on sending error code: " + errorCode + ". Save me! ");
-            }
-        });
-
-        CalllogManager.registerCalllogDataListener(new CalllogDataListener() {
-
-            @Override
-            public void callLogDataGenerated(byte[] callLogData, List<String> sessionEventIds) {
-                Logger.debug(this, "call log data: " + callLogData + " is generated");
-                _calllogSender.send(callLogData);
-            }
-        });
-
-        SessionEventBuilder appSessionEventBuilder = CalllogManager.logAppEvent("SampleAppEvent", "2.0");
-
-        if (appSessionEventBuilder != null) {
-
-            _appSessionLeadEvent = appSessionEventBuilder.commit();
-            _appLeadSessionId = _appSessionLeadEvent.getId();
-            Logger.debug(this, "application session event lead id is: " + _appLeadSessionId);
-        }
-
-        // Cloud Recognizer initialization
-        _cloudRecognizer = new CloudRecognizer(_cloudServices);
-    }
-
 
     public void startGoogleASR(View view){
 //        resultTextView.setText("");
@@ -435,14 +141,17 @@ public class CloudASRActivity extends AppCompatActivity
             ArrayList<String> textMatchlist = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
             if (!textMatchlist.isEmpty()){
-                resultTextView.setText(textMatchlist.get(0));
-                new Thread(new ClientSendThread(getAsrResultJsonStr(1, textMatchlist.get(0)))).start();
+                resultTextView.setText("ASR result: "+textMatchlist.get(0));
+                Log.d("sss", "ASR result: "+textMatchlist.get(0));
+                googleAsrResult = textMatchlist.get(0);
+                new Thread(new ClientSendThread("result-"+googleAsrResult)).start();
             }
         }
         else{
-            new Thread(new ClientSendThread(getAsrResultJsonStr(1, "Error"))).start();
+            new Thread(new ClientSendThread("error")).start();
+//            startNuanceAsrButton.performClick();
         }
-        super.onActivityResult(requestCode, resultCode, data);
+//        super.onActivityResult(requestCode, resultCode, data);
     }
 
     void  showToastMessage(String message){
@@ -495,15 +204,18 @@ public class CloudASRActivity extends AppCompatActivity
         public void run() {
             try {
                 while(clientSocket.isConnected()){
-                    String msg = clientReader.readLine();
+                    messageFromServer = clientReader.readLine();
+                    Log.d("sss", "from server: "+messageFromServer);
                     Message cmd = new Message();
-                    if(msg.equals("Google")){
-                        cmd.what = 0;
-                    }
-                    if(msg.equals("Nuance")){
+                    if(messageFromServer.equals("rev")) {
                         cmd.what = 1;
+                        severMessageHandler.sendMessage(cmd);
                     }
-                    severMessageHandler.sendMessage(cmd);
+                    if(messageFromServer.equals("finish")) {
+                        cmd.what = 4;
+                        severMessageHandler.sendMessage(cmd);
+//                        break;
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
